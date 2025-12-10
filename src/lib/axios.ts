fa7681c205628c1c743a7435650c810b64cd2bb3
Nehakumari1208 <nknehanisha@gmail.com>
2025-12-10 15:35:15 +0530

    removed comments

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,33 +5,7 @@ const axiosInstance = axios.create({
   headers: {
     Accept: 'application/json',
   },
-  withCredentials: true, // if your app uses cookies/session
+  withCredentials: true,
 });
 
 export default axiosInstance;
-
-// // utils/useAuthorizedAxios.ts
-// import axios from 'axios';
-// import { useAuth } from '@clerk/nextjs';
-
-// export const useAuthorizedAxios = () => {
-//   const { getToken } = useAuth();
-
-//   const axiosInstance = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//     withCredentials: true,
-//   });
-
-//   axiosInstance.interceptors.request.use(async (config) => {
-//     const token = await getToken();
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   });
-
-//   return axiosInstance;
-// };
